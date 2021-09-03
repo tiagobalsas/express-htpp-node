97f2560d68e70129dea04b86e25fa85350ce8697
Tiago Santos <tiago881@gmail.com>
2021-09-03 10:57:41 -0300

    pesquisa por params informado na url

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require('express');
 
 const app = express();
 
-const drinks = [
-  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
-  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
-  { id: 3, name: 'Suco 300ml', price: 4.0 },
-  { id: 4, name: 'Suco 1l', price: 10.0 },
-  { id: 5, name: 'Cerveja Lata', price: 4.5 },
-  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
-app.get('/drinks', (req, res) => {
-  res.json(drinks);
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+
+  const recipe = recipes.find((rec) => rec.id === parseInt(id));
+  if (!recipe) return res.status(404).json({ message: 'Recipe note found!' });
+
+  res.status(200).json(recipes);
 });
 
 app.listen(3001, () => {
