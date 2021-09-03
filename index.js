2be77f3bff7ad7c449884e3189de45707aa544a8
Tiago Santos <tiago881@gmail.com>
2021-09-03 10:05:23 -0300

    cria endpoint que retorna uma lista de receitas

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const app = express(); // 1
 
-app.get('/', helloWorld); // 2
+const app = express();
+
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
+
+app.get('/recipes', (req, res) => {
+  res.json(recipes);
+});
 
 app.listen(3001, () => {
   console.log('Server Online');
-}); // 3
-
-function helloWorld(_req, res) {
-  res.status(200).send('Hello, Wolrd!') // 4
-};
+});
 
 // 1. Criar uma nova aplicação Express;
-// 2. Dizer ao Express que, quando uma requisição com método GET for recebida no caminho /hello , a função helloWorld deve ser chamada;
-// 3. Pedir ao Express que crie um servidor HTTP e escute por requisições na porta 3001;
-// 4. Ao tratar uma requisição com método GET no caminho /hello , enviar o status HTTP 200 que significa OK e a mensagem "Hello world!".
+// 2. Dizer ao Express que, quando uma requisição com método GET for recebida no caminho /recipes , retorne um objeto json
+// 3. Pedir ao Express que crie um servidor HTTP e escute por requisições na porta 3001;rs
