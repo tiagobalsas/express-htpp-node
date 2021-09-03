f77b262769bc0a7a1f48fdaa3ef69cad441fd58d
Tiago Santos <tiago881@gmail.com>
2021-09-03 10:22:48 -0300

    retorna uma lista de drinks

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const express = require('express');
 
 const app = express();
 
-const recipes = [
-  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
-app.get('/recipes', (req, res) => {
-  res.json(recipes);
+app.get('/drinks', (req, res) => {
+  res.json(drinks);
 });
 
 app.listen(3001, () => {
   console.log('Server Online');
 });
-
-// 1. Criar uma nova aplicação Express;
-// 2. Dizer ao Express que, quando uma requisição com método GET for recebida no caminho /recipes , retorne um objeto json
-// 3. Pedir ao Express que crie um servidor HTTP e escute por requisições na porta 3001;rs
